b4ab8554ad3a68a962f930c0273f6775bfc7b7ba
Jangrasourabh <sourabhjangra6226@gmail.com>
2024-12-05 15:11:10 +0530

    add:all-test-cases

---
+++ src/components/StringCalculator.test.js
@@ -0,0 +1,27 @@
+test("it should return 0 when the input is an empty string", () => {
+    throw new Error();
+})
+
+test('it should return the number itself when the input is a single number', () => {
+    throw new Error();
+})
+
+test('it should return the sum of two numbers separated by a comma', () => {
+    throw new Error();
+})
+
+test('it should return the sum of numbers separated by newlines', () => {
+    throw new Error();
+})
+
+test('it should support custom delimiters', () => {
+    throw new Error();
+})
+
+test('it should throw an error for negative numbers', () => {
+    throw new Error();
+})
+
+test('it should throw an error for multiple negative numbers', () => {
+    throw new Error();
+})
